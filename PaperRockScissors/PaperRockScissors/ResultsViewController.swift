dec14995f65b20dc748bcb8a9078e758e887f7b3
Andre Fichel <fichel@apple.com>
2016-06-13 15:39:35 -0700

    Adds changes to PSR and includes example solution

--- PaperRockScissors/PaperRockScissors/ResultsViewController.swift
+++ PaperRockScissors/PaperRockScissors/ResultsViewController.swift
@@ -15,7 +15,7 @@ class ResultsViewController: UIViewController {
         case Rock
         case Scissors
         
-        func randomChoice() -> Results {
+        static func randomChoice() -> Results {
             let rand = arc4random_uniform(3)
             return Results(rawValue: rand)!
         }
@@ -29,31 +29,9 @@ class ResultsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let uc = self.userChoice, oc = self.opponentChoice {
-            switch (uc, oc) {
-            case (.Paper, .Rock):
-                resultsImage.image = UIImage(named: "PaperCoversRock")
-                resultsLabel.text = "You won! Paper covers rock."
-            case (.Rock, .Paper):
-                resultsImage.image = UIImage(named: "PaperCoversRock")
-                resultsLabel.text = "You lost... Paper covers rock."
-            case (.Paper, .Scissors):
-                resultsImage.image = UIImage(named: "ScissorsCutPaper")
-                resultsLabel.text = "You won! Scissors cuts paper."
-            case (.Scissors, .Paper):
-                resultsImage.image = UIImage(named: "ScissorsCutPaper")
-                resultsLabel.text = "You lost... Scissors cuts paper."
-            case (.Scissors, .Rock):
-                resultsImage.image = UIImage(named: "RockCrushesScissors")
-                resultsLabel.text = "You won! Rock crushes scissors"
-            case (.Rock, .Scissors):
-                resultsImage.image = UIImage(named: "RockCrushesScissors")
-                resultsLabel.text = "You lost... Rock crushes scissors"
-            default:
-                resultsImage.image = UIImage(named: "itsATie")
-                resultsLabel.text = "It's a tie! Best of 3?"
-            }
+            determineWinner(user: uc, opponent: oc)
         }
     }
     
@@ -62,7 +40,34 @@ class ResultsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func determineWinner(user: Results, opponent: Results) {
+        switch (user, opponent) {
+        case (.Paper, .Rock):
+            resultsImage.image = UIImage(named: "PaperCoversRock")
+            resultsLabel.text = "You won! Paper covers rock."
+        case (.Rock, .Paper):
+            resultsImage.image = UIImage(named: "PaperCoversRock")
+            resultsLabel.text = "You lost... Paper covers rock."
+        case (.Scissors, .Paper):
+            resultsImage.image = UIImage(named: "ScissorsCutPaper")
+            resultsLabel.text = "You won! Scissors cuts paper."
+        case (.Paper, .Scissors):
+            resultsImage.image = UIImage(named: "ScissorsCutPaper")
+            resultsLabel.text = "You lost... Scissors cuts paper."
+        case (.Rock, .Scissors):
+            resultsImage.image = UIImage(named: "RockCrushesScissors")
+            resultsLabel.text = "You won! Rock crushes scissors"
+        case (.Scissors, .Rock):
+            resultsImage.image = UIImage(named: "RockCrushesScissors")
+            resultsLabel.text = "You lost... Rock crushes scissors"
+        default:
+            resultsImage.image = UIImage(named: "itsATie")
+            resultsLabel.text = "It's a tie! Best of 3?"
+        }
+    }
+    
     @IBAction func playAgain(sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
     }
 
     /*
