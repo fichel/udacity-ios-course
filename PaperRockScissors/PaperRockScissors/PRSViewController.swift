ba57d60c899e852b524c14590cd8a8bed7c93065
Andre Nascimento <andre_nascimento@apple.com>
2016-06-13 21:39:15 -0700

    Adds dumb solution for PRS

--- PaperRockScissors/PaperRockScissors/PRSViewController.swift
+++ PaperRockScissors/PaperRockScissors/PRSViewController.swift
@@ -20,33 +20,35 @@ class PRSViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func chooseRock(sender: AnyObject) {
+    @IBAction private func chooseRock(sender: AnyObject) {
         
         var controller: ResultsViewController
         controller = self.storyboard?.instantiateViewController(withIdentifier: "ResultsViewController") as! ResultsViewController
-        
         controller.userChoice = .Rock
         controller.opponentChoice = ResultsViewController.Results.randomChoice()
-        
         self.present(controller, animated: true, completion: nil)
     }
 
-    @IBAction func choosePaper(sender: AnyObject) {
-        self.performSegue(withIdentifier: "choosePaper", sender: self)
+    @IBAction private func choosePaper(sender: AnyObject) {
+        self.performSegue(withIdentifier: "showResults", sender: sender)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
-        switch segue {
-        case "choosePaper":
-            let controller = segue.destinationViewController as! ResultsViewController
-            controller.userChoice = .Paper
-            controller.opponentChoice = ResultsViewController.Results.randomChoice()
-        case "chooseScissors":
-            let controller = segue.destinationViewController as! ResultsViewController
-            controller.userChoice = .Scissors
-            controller.opponentChoice = ResultsViewController.Results.randomChoice()
-        default:
-            break
+        if segue.identifier == "showResults" {
+            let choice = sender?.tag
+            switch choice! {
+            case 1:
+                let controller = segue.destinationViewController as! ResultsViewController
+                controller.userChoice = .Paper
+                controller.opponentChoice = ResultsViewController.Results.randomChoice()
+            case 2:
+                let controller = segue.destinationViewController as! ResultsViewController
+                controller.userChoice = .Scissors
+                controller.opponentChoice = ResultsViewController.Results.randomChoice()
+            default:
+                break
+            }
+            
         }
     }
 }
